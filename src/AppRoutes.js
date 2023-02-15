fb2c4e2ad4915b8328f29ae5a61189e74ea7871b
Anna Lepak <anna.lepak@adesso.de>
2023-02-15 17:37:41 +0100

    added details page and error page

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react"
 import { Route, Routes } from "react-router-dom";
+import Err404Page from "./components/Pages/404ErrPage";
 import PostListPage from "./components/Pages/PostListPage";
+import PostDetailPage from "./components/PostDetailPage/PostDetailPage";
 import Login from "./container/Login/Login";
 import PostForm from "./container/PostForm/PostForm";
 import { StateContext } from "./state/context/context";
@@ -13,7 +15,8 @@ function AppRoutes() {
         <Route path={"/"} element={<PostListPage />} />
         <Route path={"/login"} element={<Login />} />
         <Route path={"/newPost"} element={<PostForm state={state} dispatch={dispatch} />} />
-
+        <Route path={"/post/:idx"} element={<PostDetailPage state={state} dispatch={dispatch} />} />
+        <Route path={"*"} element={ <Err404Page /> } />
     </Routes>
   )
 };
